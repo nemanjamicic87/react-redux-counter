74b6e6040f9cf2bb52cd923b99482140856b5bfe
Nemanja Micic <nemanja.micic@novalite.rs>
2019-04-11 14:11:31 +0200

    code refactoring: li key bind name changed

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -18,7 +18,7 @@ class Counter extends Component {
                 <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
-                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                        <li key={storedResults.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
                     ))}
                 </ul>
             </div>
